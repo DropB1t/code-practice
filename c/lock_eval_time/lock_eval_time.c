6750c680dcd1728dc496ef9c72a53b197ec57b91
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2023-03-06 00:40:49 +0100

    release: lock time evaluation with N threads

--- c/lock_eval_time/lock_eval_time.c
+++ c/lock_eval_time/lock_eval_time.c
@@ -1,8 +1,29 @@
-#include <pthread.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
 #include <sys/time.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define ITERATIONS 5000000
+#define N_THREAD 50
+
+int resource;
+pthread_mutex_t lock;
+
+void check(int test, const char *message, ...)
+{
+	if (test)
+	{
+		va_list args;
+		va_start(args, message);
+		vfprintf(stderr, message, args);
+		va_end(args);
+		fprintf(stderr, "\n");
+		exit(EXIT_FAILURE);
+	}
+}
 
 long delta_time(struct timeval *now, struct timeval *before)
 {
@@ -21,37 +42,65 @@ long delta_time(struct timeval *now, struct timeval *before)
 	return ((delta_seconds * 1000000) + delta_microseconds);
 }
 
-int main(int argc, char const *argv[])
+void *Worker(void *arg)
 {
-	const long N = 50000000;
 
 	struct timeval start, stop;
-	long double us;
-
-	pthread_mutex_t lock;
 
 	if (gettimeofday(&start, NULL) == -1)
 	{
 		perror("gettimeofday start");
-		return EXIT_FAILURE;
+		return 0;
 	}
 
-	for (long i = 0; i < N; i++)
+	for (long i = 0; i < ITERATIONS; i++)
 	{
 		pthread_mutex_lock(&lock);
+		resource++;
 		pthread_mutex_unlock(&lock);
 	}
 
 	if (gettimeofday(&stop, NULL) == -1)
 	{
 		perror("gettimeofday close");
-		return EXIT_FAILURE;
+		return 0;
 	}
 
-	us = (long double)delta_time(&stop,&start);
+	double *us = malloc(sizeof(double));
+	*us = (double)delta_time(&stop, &start);
 
-	printf("\nWhole for loop: %.0Lf us\n", us);
-	printf("Each lock: %Lf us\n", us / N);
+	return (void *)us;
+}
+
+int main(int argc, char const *argv[])
+{
+	int err;
+	pthread_t th[N_THREAD];
+	long res[N_THREAD];
+
+	for (int i = 0; i < N_THREAD; i++)
+	{
+		err = pthread_create(&th[i], NULL, Worker, NULL);
+		check(err != 0, "pthread_create failed (Worker n.%ld): %s", i, strerror(err));
+	}
+
+	double *us;
+	double sum = 0.0;
+	for (int i = 0; i < N_THREAD; i++)
+	{
+		err = pthread_join(th[i], (void **)&us);
+		check(err != 0, "pthread_join failed (Worker n.%ld): %s", i, strerror(err));
+
+		printf("\nThread n.%d\n", i + 1);
+		printf("For loop: %.5f s [%.0f ms] [%.0f us]\n", *us / 1e+6, *us / 1000, *us);
+		printf("For each lock: %f us\n", *us / ITERATIONS);
+
+		sum += (*us / ITERATIONS);
+		free(us);
+	}
+
+	sum = sum / N_THREAD;
+	printf("\nAvg lock time [plus int increament]: %f\n", sum);
 
 	return EXIT_SUCCESS;
 }
