54761813bf5d875f3ae79ecd7cbe5df4a32731a2
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2023-03-05 22:26:02 +0100

    lock_eval_time(c): delta_time function

--- c/lock_eval_time/lock_eval_time.c
+++ c/lock_eval_time/lock_eval_time.c
@@ -4,36 +4,54 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+long delta_time(struct timeval *now, struct timeval *before)
+{
+	time_t delta_seconds;
+	time_t delta_microseconds;
+
+	delta_seconds = now->tv_sec - before->tv_sec;
+	delta_microseconds = now->tv_usec - before->tv_usec;
+
+	if (delta_microseconds < 0)
+	{
+		delta_microseconds += 1000000;
+		--delta_seconds;
+	}
+
+	return ((delta_seconds * 1000000) + delta_microseconds);
+}
+
 int main(int argc, char const *argv[])
 {
-  const long N = 5000000;
+	const long N = 50000000;
 
-  struct timeval start, stop;
-  long double us;
+	struct timeval start, stop;
+	long double us;
 
-  pthread_mutex_t lock;
+	pthread_mutex_t lock;
 
-  if (gettimeofday(&start, NULL) == -1)
-  {
-    perror("gettimeofday start");
-    return EXIT_FAILURE;
-  }
+	if (gettimeofday(&start, NULL) == -1)
+	{
+		perror("gettimeofday start");
+		return EXIT_FAILURE;
+	}
 
-  for (long i = 0; i < N; i++)
-  {
-    pthread_mutex_lock(&lock);
-    pthread_mutex_unlock(&lock);
-  }
+	for (long i = 0; i < N; i++)
+	{
+		pthread_mutex_lock(&lock);
+		pthread_mutex_unlock(&lock);
+	}
 
-  if (gettimeofday(&stop, NULL) == -1)
-  {
-    perror("gettimeofday close");
-    return EXIT_FAILURE;
-  }
+	if (gettimeofday(&stop, NULL) == -1)
+	{
+		perror("gettimeofday close");
+		return EXIT_FAILURE;
+	}
 
-  us = (long double)(stop.tv_sec * 1000000L + stop.tv_usec) - (start.tv_sec * 1000000L + start.tv_usec);
+	us = (long double)delta_time(&stop,&start);
 
-  printf("%Lf\n", us / N);
+	printf("\nWhole for loop: %.0Lf us\n", us);
+	printf("Each lock: %Lf us\n", us / N);
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
