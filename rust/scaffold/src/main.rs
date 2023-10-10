9c1023728636a2a1313b757d8089415846e460d1
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2023-10-10 23:26:56 +0200

    refactor: better binary-tree add impl

--- rust/scaffold/src/main.rs
+++ rust/scaffold/src/main.rs
@@ -1,7 +1,6 @@
 use rand::Rng;
 use std::mem;
 
-
 #[derive(Debug)]
 struct Node {
     val: i32,
@@ -10,51 +9,65 @@ struct Node {
 }
 
 impl Node {
-    fn add(&mut self, x:i32){
+    fn add(&mut self, x: i32) {
         if x > self.val {
-            if let Some( rn) = self.right.as_mut() {
-                rn.add(x)
-            }else {
-                self.right = Some(Box::new(Node{val:x,right:None,left:None}));
+            match &mut self.right {
+                Some(r) => (*r).add(x),
+                None => {
+                    self.right = Some(Box::new(Node {
+                        val: x,
+                        right: None,
+                        left: None,
+                    }))
+                }
             }
-        } else {
-            if let Some(ln) = self.left.as_mut() {
-                ln.add(x)
-            }else {
-                self.left = Some(Box::new(Node{val:x,right:None,left:None}));
+            return;
+        }
+
+        match &mut self.left {
+            Some(l) => (*l).add(x),
+            None => {
+                self.left = Some(Box::new(Node {
+                    val: x,
+                    right: None,
+                    left: None,
+                }))
             }
         }
     }
 
     fn get_size(&self) -> usize {
-        let mut s:usize = 0;
+        let mut s: usize = 0;
         s += mem::size_of_val(self);
-        if let Some(rn) = self.right.as_ref() {
-            s += rn.get_size();
+
+        match &self.right {
+            Some(r) => s += (*r).get_size(),
+            None => (),
         }
-        if let Some(ln) = self.left.as_ref() {
-            s += ln.get_size();
+
+        match &self.left {
+            Some(l) => s += (*l).get_size(),
+            None => (),
         }
+
         return s;
     }
 }
 
 fn main() {
-
-    let mut tree: Node = Node{val:2,right:None,left:None};
+    let mut tree: Node = Node {
+        val: 0,
+        right: None,
+        left: None,
+    };
     let mut rng = rand::thread_rng();
 
-    let mut i:i32 = 1;
+    let mut i: i32 = 100;
     while i != 0 {
-        tree.add(rng.gen::<i32>());
-        i-=1;
+        tree.add(rng.gen_range(-500..=500));
+        i -= 1;
     }
 
-    println!("Tree occupies {} bytes on the stack", tree.get_size());
-
-    //println!("{:?}", n);
-
-    /* let s = from_utf8(&[240,159,141,137]);
-    println!("{}",s.unwrap()); */
-    
+    println!("{:?}", tree);
+    println!("Tree occupies {} bytes", tree.get_size());
 }
